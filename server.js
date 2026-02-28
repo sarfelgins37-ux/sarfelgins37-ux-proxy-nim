195fa6f69a2f6c9891b6a93d27507ea9c10f15a6
sarfelgins37-ux <sarfelgins37@gmail.com>
2026-02-28 13:16:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
